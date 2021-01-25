d3173035402835247025b336bf3e3026e1d833df
icedjello <bamfard@gmail.com>
2021-01-25 10:51:19 +0000

    Commit with selected, more rows, and ability  to highlight based on price.

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -6,35 +6,95 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import 'ag-grid-enterprise'
 
 const initialRowData = [
-    {id: 0, make: "Toyota", model: "Celica", multi: {price: 15000, style: null}},
-    {id: 1, make: "Ford", model: "Mondeo", multi: {price: 32000, style: null}},
-    {id: 2, make: "Porsche", model: "Boxter", multi: {price: 72000, style: null}}
+    {id: 0, brand: {name: "Toyota", selected: false}, model: "Celica", price: {amount: 15000, style: null}},
+    {id: 1, brand: {name: "Ford", selected: false}, model: "Mondeo", price: {amount: 32000, style: null}},
+    {id: 2, brand: {name: "Porsche", selected: false}, model: "Boxter", price: {amount: 72000, style: null}},
+    {id: 3, brand: {name: "Ford", selected: false}, model: "Mustang", price: {amount: 60000, style: null}},
+    {id: 4, brand: {name: "Toyota", selected: false}, model: "Camry", price: {amount: 20000, style: null}},
+    {id: 5, brand: {name: "Porsche", selected: false}, model: "Cayenne", price: {amount: 50000, style: null}},
+    {id: 6, brand: {name: "Suzuki", selected: false}, model: "Swift", price: {amount: 10000, style: null}},
+    {id: 7, brand: {name: "Suzuki", selected: false}, model: "Jimny", price: {amount: 20000, style: null}},
+    {id: 8, brand: {name: "Toyota", selected: false}, model: "Prius", price: {amount: 18000, style: null}},
+    {id: 9, brand: {name: "Ford", selected: false}, model: "Fiesta", price: {amount: 16000, style: null}},
+    {id: 10, brand: {name: "Toyota", selected: false}, model: "Supra", price: {amount: 72000, style: null}},
+    {id: 11, brand: {name: "Porsche", selected: false}, model: "Cayman", price: {amount: 88000, style: null}},
+    {id: 12, brand: {name: "Porsche", selected: false}, model: "Spyder", price: {amount: 73000, style: null}},
 ]
 
+const SELECTION = 'selection';
 const LOW = 'low'
 const MEDIUM = 'medium'
 const HIGH = 'high'
 
 const reducer = (state, action) => {
-    console.log(action.style)
-    console.log(action.id)
     const newState = [...state];
-
-    return newState.map(
-        it => {
-            if (it.id === action.id) {
-                return {
-                    ...it,
-                    multi: {
-                        ...it.multi,
-                        style: action.style
+    switch (action.type) {
+        case SELECTION:
+            return newState.map(
+                row => {
+                    if (row.id === action.id) {
+                        return {
+                            ...row,
+                            brand: {
+                                ...row.brand,
+                                selected: action.selected
+                            }
+                        }
                     }
+                    return row;
                 }
-            }
-            return it;
-        }
-    );
-
+            );
+        case LOW:
+            return newState.map(
+                row => {
+                    const price = row.price.amount;
+                    if (price < 20000) {
+                        return {
+                            ...row,
+                            price: {
+                                ...row.price,
+                                style: LOW
+                            }
+                        }
+                    }
+                    return row;
+                }
+            );
+        case MEDIUM:
+            return newState.map(
+                row => {
+                    const price = row.price.amount;
+                    if (price >= 20000 && price < 55000) {
+                        return {
+                            ...row,
+                            price: {
+                                ...row.price,
+                                style: MEDIUM
+                            }
+                        }
+                    }
+                    return row;
+                }
+            );
+        case HIGH:
+            return newState.map(
+                row => {
+                    const price = row.price.amount;
+                    if (price >= 55000) {
+                        return {
+                            ...row,
+                            price: {
+                                ...row.price,
+                                style: HIGH
+                            }
+                        }
+                    }
+                    return row;
+                }
+            );
+        default:
+            console.warn('No matching reducer case.')
+    }
 }
 
 
@@ -48,7 +108,7 @@ export function Grid() {
     }
 
     const GRID_STYLES = {
-        height: 200,
+        height: 600,
         width: 600,
         marginTop: 5,
         marginLeft: 'auto',
@@ -56,20 +116,32 @@ export function Grid() {
     }
 
     const cellStyler = params => {
-        switch (params.data.multi.style) {
-            case LOW:
-                return {backgroundColor: 'green'};
-            case MEDIUM:
-                return {backgroundColor: 'yellow'};
-            case HIGH:
-                return {backgroundColor: 'red'};
+        switch (params.data.brand.selected) {
+            case true:
+                return {backgroundColor: 'lightblue'};
             default:
                 return null;
         }
     }
 
+    const priceClassRules = {
+        'rag-green': function (params) {
+            return params.data.price.style === LOW;
+        },
+        'rag-amber': function (params) {
+            return params.data.price.style === MEDIUM;
+        },
+        'rag-red': function (params) {
+            return params.data.price.style === HIGH;
+        },
+    }
+
     const priceValueGetter = (params) => {
-        return params.data.multi.price
+        return params.data.price.amount
+    }
+
+    const makeValueGetter = (params) => {
+        return params.data.brand.name
     }
 
     const onSelectionChanged = () => {
@@ -77,23 +149,34 @@ export function Grid() {
         setSelectedRowId(selectedRows[0].id)
     };
 
-    const buttonClickHandler = (style) => {
+    const buttonSelectClickHandler = () => {
         if (!(selectedRowId == null)) {
             const action = {
-                style: style,
+                type: SELECTION,
+                selected: true,
                 id: selectedRowId
             }
             dispatch(action)
         }
     }
 
+    const buttonHighlightHandler = (style) => {
+        console.log('select', style)
+        const action = {
+            type: style,
+        }
+        dispatch(action)
+    }
 
     return (
         <div>
+            <span style={{display: "block"}}>
+                <button onClick={() => buttonSelectClickHandler()}>Select</button>
+            </span>
             <span>
-                <button onClick={() => buttonClickHandler(LOW)}>low</button>
-                <button onClick={() => buttonClickHandler(MEDIUM)}>medium</button>
-                <button onClick={() => buttonClickHandler(HIGH)}>high</button>
+                 <button onClick={() => buttonHighlightHandler(LOW)}>low</button>
+                 <button onClick={() => buttonHighlightHandler(MEDIUM)}>medium</button>
+                 <button onClick={() => buttonHighlightHandler(HIGH)}>high</button>
             </span>
             <div
                 className="ag-theme-alpine"
@@ -105,13 +188,20 @@ export function Grid() {
                     onSelectionChanged={onSelectionChanged}
 
                 >
-                    <AgGridColumn field="make"/>
+                    <AgGridColumn
+                        field="brand"
+                        headerName="Make"
+                        valueGetter={makeValueGetter}
+                        cellStyle={params => cellStyler(params)}
+
+                    />
                     <AgGridColumn field="model"/>
                     <AgGridColumn
                         field="multi"
                         headerName="Price"
                         valueGetter={priceValueGetter}
-                        cellStyle={params => cellStyler(params)}
+                        cellClassRules={priceClassRules}
+
                     />
                 </AgGridReact>
             </div>
